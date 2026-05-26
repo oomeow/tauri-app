1ed5fcd43c6ddfd17749f08c0d3f16f0fa35a888
oomeow <oomeow@outlook.com>
2026-05-26 23:30:03 +0800

    init

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,110 +1,88 @@
-use std::time::Duration;
-
-use tauri::{
-    menu::{Menu, MenuItem},
-    tray::{MouseButton, TrayIconBuilder},
-    Manager, WebviewUrl, WebviewWindowBuilder,
+use std::{
+    io::Write,
+    sync::atomic::{AtomicBool, Ordering},
 };
+
+use rquickjs::{CatchResultExt, Context, Function, Object, Runtime, Value};
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
-async fn greet(name: String) -> String {
-    // Suppose it took 5 seconds to perform other operations, like fetching data from an API.
-    std::thread::sleep(Duration::from_secs(5));
+fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+fn print(s: String) {
+    println!("{s}");
+}
+
+#[tauri::command]
+fn run_js() -> Result<(), String> {
+    let rt = Runtime::new().map_err(|e| e.to_string())?;
+    let ctx = Context::full(&rt).map_err(|e| e.to_string())?;
+    let should_exit = std::sync::Arc::new(AtomicBool::new(false));
+
+    ctx.with(|ctx| -> Result<(), String> {
+        let global = ctx.globals();
+        let should_exit_clone = should_exit.clone();
+
+        global
+            .set(
+                "__print",
+                Function::new(ctx.clone(), print)
+                    .map_err(|e| e.to_string())?
+                    .with_name("__print")
+                    .map_err(|e| e.to_string())?,
+            )
+            .map_err(|e| e.to_string())?;
+
+        global
+            .set(
+                "exit",
+                Function::new(ctx.clone(), move || {
+                    should_exit_clone.store(true, Ordering::Relaxed);
+                })
+                .map_err(|e| e.to_string())?,
+            )
+            .map_err(|e| e.to_string())?;
+
+        ctx.eval::<(), _>(
+            r#"
+globalThis.console = {
+  log(...v) {
+    globalThis.__print(`${v.join(" ")}`)
+  }
+}
+"#,
+        )
+        .map_err(|e| e.to_string())?;
+
+        let console: Object = global.get("console").map_err(|e| e.to_string())?;
+        let js_log: Function = console.get("log").map_err(|e| e.to_string())?;
+        loop {
+            if should_exit.load(Ordering::Relaxed) {
+                break;
+            }
+            let mut input = String::new();
+            print!("> ");
+            std::io::stdout().flush().map_err(|e| e.to_string())?;
+            std::io::stdin()
+                .read_line(&mut input)
+                .map_err(|e| e.to_string())?;
+            ctx.eval::<Value, _>(input.as_bytes())
+                .and_then(|ret| js_log.call::<(Value<'_>,), ()>((ret,)))
+                .catch(&ctx)
+                .unwrap_or_else(|err| println!("{err}"));
+        }
+        Ok(())
+    })?;
+    Ok(())
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_window_state::Builder::new().build())
-        .plugin(tauri_plugin_shell::init())
-        .setup(|app| {
-            let show_window = MenuItem::with_id(app, "show", "Show", true, None::<&str>).unwrap();
-            let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>).unwrap();
-            let menu = Menu::with_items(app, &[&show_window, &quit_i]).unwrap();
-            let _ = TrayIconBuilder::new()
-                .icon(app.default_window_icon().unwrap().clone())
-                .on_tray_icon_event(|tray, event| match event {
-                    tauri::tray::TrayIconEvent::Click {
-                        button: MouseButton::Left,
-                        ..
-                    } => {
-                        let main_window = tray.app_handle().get_webview_window("main");
-                        match main_window {
-                            Some(window) => {
-                                let _ = window.unminimize();
-                                let _ = window.show();
-                                let _ = window.set_focus();
-                            }
-                            None => {
-                                let window = WebviewWindowBuilder::new(
-                                    tray.app_handle(),
-                                    "main",
-                                    WebviewUrl::default(),
-                                )
-                                .visible(false)
-                                .transparent(true)
-                                .shadow(false)
-                                .build()
-                                .unwrap();
-                                let _ = window.show();
-                                let _ = window.set_focus();
-                            }
-                        }
-                    }
-                    _ => {}
-                })
-                .menu(&menu)
-                .menu_on_left_click(false)
-                .on_menu_event(|app_handle, event| match event.id.as_ref() {
-                    "show" => match app_handle.get_webview_window("main") {
-                        Some(window) => {
-                            let _ = window.unminimize();
-                            let _ = window.show();
-                            let _ = window.set_focus();
-                        }
-                        None => {
-                            let window = WebviewWindowBuilder::new(
-                                app_handle,
-                                "main",
-                                WebviewUrl::default(),
-                            )
-                            .visible(false)
-                            .transparent(true)
-                            .shadow(false)
-                            .build()
-                            .unwrap();
-                            let _ = window.show();
-                            let _ = window.set_focus();
-                        }
-                    },
-                    "quit" => {
-                        app_handle.exit(0);
-                        std::process::exit(0);
-                    }
-                    _ => {}
-                })
-                .build(app)
-                .unwrap();
-
-            let window = WebviewWindowBuilder::new(app, "main", tauri::WebviewUrl::default())
-                .min_inner_size(800., 600.)
-                .visible(false)
-                .center()
-                .build()
-                .unwrap();
-            let _ = window.show();
-            let _ = window.set_focus();
-            Ok(())
-        })
-        .invoke_handler(tauri::generate_handler![greet])
-        .build(tauri::generate_context!())
-        .expect("error while running tauri application")
-        .run(|_app_handle, event| match &event {
-            tauri::RunEvent::ExitRequested { api, .. } => {
-                api.prevent_exit();
-            }
-            _ => {}
-        });
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![greet, run_js])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import { invoke } from "@tauri-apps/api/core";
-import { getCurrentWindow } from "@tauri-apps/api/window";
 import { useState } from "react";
-import "./App.css";
 import reactLogo from "./assets/react.svg";
+import { invoke } from "@tauri-apps/api/core";
+import "./App.css";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
-  const [pin, setPin] = useState(false);
-
-  const appWindow = getCurrentWindow();
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
@@ -17,88 +13,38 @@ function App() {
   }
 
   return (
-    <>
-      <div data-tauri-drag-region className="titlebar">
-        <div
-          className="titlebar-button"
-          id="titlebar-always-on-top"
-          onClick={() => {
-            let nextPin = !pin;
-            setPin(nextPin);
-            appWindow.setAlwaysOnTop(nextPin);
-          }}
-        >
-          <img src="https://api.iconify.design/mdi:pin.svg" alt="minimize" />
-        </div>
-        <div
-          className="titlebar-button"
-          id="titlebar-minimize"
-          onClick={() => appWindow.minimize()}
-        >
-          <img
-            src="https://api.iconify.design/mdi:window-minimize.svg"
-            alt="minimize"
-          />
-        </div>
-        <div
-          className="titlebar-button"
-          id="titlebar-maximize"
-          onClick={() => appWindow.toggleMaximize()}
-        >
-          <img
-            src="https://api.iconify.design/mdi:window-maximize.svg"
-            alt="maximize"
-          />
-        </div>
-        <div
-          className="titlebar-button"
-          id="titlebar-close"
-          onClick={() => appWindow.close()}
-        >
-          <img src="https://api.iconify.design/mdi:close.svg" alt="close" />
-        </div>
+    <main className="container">
+      <h1>Welcome to Tauri + React</h1>
+
+      <div className="row">
+        <a href="https://vite.dev" target="_blank">
+          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+        </a>
+        <a href="https://tauri.app" target="_blank">
+          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-      <main className="container">
-        <h1>Welcome to Tauri + React</h1>
+      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
 
-        <div className="row">
-          <a href="https://vitejs.dev" target="_blank">
-            <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-          </a>
-          <a href="https://tauri.app" target="_blank">
-            <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-          </a>
-          <a href="https://reactjs.org" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-        <form
-          className="row"
-          onSubmit={(e) => {
-            e.preventDefault();
-            greet();
-          }}
-        >
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="submit">Greet</button>
-        </form>
-        <p>{greetMsg}</p>
-        <div className="row" style={{ gap: "10px" }}>
-          <button onClick={() => appWindow.setDecorations(true)}>
-            Enable Decoration
-          </button>
-          <button onClick={() => appWindow.setDecorations(false)}>
-            Disable Decoration
-          </button>
-        </div>
-      </main>
-    </>
+      <form
+        className="row"
+        onSubmit={(e) => {
+          e.preventDefault();
+          greet();
+        }}
+      >
+        <input
+          id="greet-input"
+          onChange={(e) => setName(e.currentTarget.value)}
+          placeholder="Enter a name..."
+        />
+        <button type="submit">Greet</button>
+      </form>
+      <p>{greetMsg}</p>
+    </main>
   );
 }
 
